f27cb6ad29615781af23388f6c3748ead2fc06b7
Raihan Adam <hallwack.id@gmail.com>
2022-02-26 11:21:11 +0700

    Beres mkdir log; Tinggal agregasi

--- minute_log.sh
+++ minute_log.sh
@@ -1,2 +1,12 @@
-(crontab -l | grep --invert-match --fixed-strings "$(pwd)/monitoring.sh"; echo "* * * * * $(pwd)/monitoring.sh >> /home/$(whoami)/log/metrics_$(date +%Y%m%d%H%M%S).log 2>&1") | crontab -
+#!/bin/bash
+
+loc_dir=/home/$(whoami)/log # membuat variable yg isinya directory ke folder log
+
+if [[ ! -d $loc_dir ]]; then # kondisi apakah folder log sudah ada atau blm
+  mkdir $loc_dir # membuat folder log
+  (crontab -l | grep --invert-match --fixed-strings "$(pwd)/monitoring.sh"; echo "* * * * * $(pwd)/monitoring.sh >> $loc_dir/metrics_$(date +%Y%m%d%H%M%S).log 2>&1") | crontab - # menjalankan crontab
+fi
+
+(crontab -l | grep --invert-match --fixed-strings "$(pwd)/monitoring.sh"; echo "* * * * * $(pwd)/monitoring.sh >> $loc_dir/metrics_$(date +%Y%m%d%H%M%S).log 2>&1") | crontab -
+# (crontab -l | grep --invert-match --fixed-strings "$(pwd)/monitoring.sh"; echo "* * * * * $(pwd)/monitoring.sh >> /home/$(whoami)/log/metrics_$(date +%Y%m%d%H%M%S).log 2>&1") | crontab -
 # (crontab -l | grep --invert-match --fixed-strings "$(pwd)/monitoring-ram.sh"; echo "* * * * * $(pwd)/monitoring-ram.sh") | crontab -
--- monitoring.sh
+++ monitoring.sh
@@ -7,18 +7,18 @@
 #   du -sh $1
 # ) >> log_monitor_$(date +%Y%m%d%H%M%S).log
 
-information="Log File - $(date +%Y%m%d%H%M%S) - $(whoami) - $(pwd) \n"
-memory=$(free -m)
-disk_usage=$(du -sh /home/$(whoami)/Documents/Programming/Web/Vue/vue-blog-app)
+# information="Log File - $(date +%Y%m%d%H%M%S) - $(whoami) - $(pwd) \n"
+# memory=$(free -m)
+# disk_usage=$(du -sh /home/$(whoami)/Documents/Programming/Web/Vue/vue-blog-app)
 
 action ()
 {
-  printf "Log File - $(date +%Y%m%d%H%M%S) - $(whoami) - $(pwd) \n"
-  free -m
-  du -sh /home/$(whoami)
+  printf "Log File - $(date +%Y%m%d%H%M%S) - $(whoami) - $(pwd) \n" # whoami = untuk mengetahui nama user; pwd = untuk mengetahui sedang berada di directory mana
+  free -m # free = command untuk mengetahui penggunaan memory; flag -m = penghitungan memory dengan satuan mebibyte
+  du -sh /home/$(whoami) # du = command untuk mengetahui penyimpanan yang terpakai; flag -s = menghitung total keseluruhan direktori; flag -h = human-readable format, format yang bisa dibaca manusia
 }
 
-action
+action # memanggil function action
 
 # (crontab -l | grep --invert-match --fixed-strings "$information; free -m; du -sh /home/hallwack/Documents/Programming/Web/Vue/vue-blog-app;"; echo "* * * * * $information; free -m; du -sh /home/hallwack/Documents/Programming/Web/Vue/vue-blog-app; >> $(pwd)/metrics_$(date +%Y%m%d%H%M%S).log 2>&1") | crontab -
 # 1 * * * * action $1 >> log_monitor_ahahha.log
